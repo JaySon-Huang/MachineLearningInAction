71cfc838c24d5272e8c20431c2ad5d29cefbb096
JaySon-Huang <jayson.hjs@gmail.com>
2016-01-24 02:58:48 +0800

    决策树

---
+++ ch03 - DicisionTree/dicisiontree.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+from __future__ import print_function
+
+import math
+import operator
+import pickle
+from collections import defaultdict
+
+import numpy as np
+
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(levelname)s %(module)s line:%(lineno)d] %(message)s',
+)
+
+class Dataset(object):
+    """ 对数据集以及相关操作的封装 """
+
+    def __init__(self, rawDataset):
+        self.rawDataset = np.array(rawDataset)
+
+    @property
+    def shape(self):
+        return self.rawDataset.shape
+
+    @property
+    def classList(self):
+        return self.rawDataset[:, -1].tolist()
+
+    @property
+    def shannonEntropy(self):
+        """获取数据集的香农熵
+        熵越大代表混乱程度越高, 即混合的数据越多
+        
+        Returns
+        -------
+        float : 数据集的香农熵
+        """
+        # 统计每个 label 出现的次数
+        labelCounts = defaultdict(int)
+        for featVec in self.rawDataset:
+            label = featVec[-1]
+            labelCounts[label] += 1
+        # 计算熵
+        # H = - ∑(n, i=1) ( p(xi) * log(2)(p(xi)) )
+        entropy = 0.0
+        numEntries = len(self.rawDataset)
+        for label in labelCounts:
+            probability = 1.0*labelCounts[label] / numEntries
+            entropy -= probability * math.log(probability, 2)  # 底数为2
+        return entropy
+
+    def split(self, axis):
+        """ 对数据集按照 axis 指定的特征进行划分
+
+        Parameters
+        ----------
+        axis : int
+            指定用于进行划分的特征
+
+        Returns
+        -------
+        按照指定特征划分后的 (值, 子数据集) 对
+        """
+        subDatasets = defaultdict(list)
+        for featureVector in self.rawDataset:
+            value = featureVector[axis]
+            subFeatureVector = (
+                featureVector[:axis].tolist()
+                + featureVector[axis+1:].tolist()
+            )  # 去除已经用于划分的特征
+            subDatasets[value].append(subFeatureVector)
+        return (
+            list(subDatasets.keys()),
+            list(map(self.__class__, subDatasets.values())),
+        )
+
+    def ChooseBestSplitFeature(self):
+        """通过遍历数据集, 计算香农熵, 选择最好的特征进行数据划分
+
+        Returns
+        -------
+        int : 对数据熵增益最大的划分特征的index
+        """
+        m, n = self.shape
+        numFeatures = n - 1
+        baseEntropy = self.shannonEntropy  # 当前整个数据集的熵
+        best = {
+            'gain': 0.0,  # 记录最好的信息增益
+            'feature': -1,  # 记录最好的特征index
+        }
+        # 按照不同的特征遍历进行划分
+        for featureIndex in range(numFeatures):
+            _labels, subDatasets = self.split(featureIndex)
+            # 计算按照此特征进行划分后的熵
+            newEntropy = 0.0
+            for subDataset in subDatasets:
+                sub_m, _sub_n = subDataset.shape
+                probability = 1.0*sub_m / m
+                newEntropy += probability * subDataset.shannonEntropy
+            # 计算信息增益, 更新最好的特征
+            infoGain = baseEntropy - newEntropy
+            if infoGain > best['gain']:
+                best['gain'] = infoGain
+                best['feature'] = featureIndex
+        return best['feature']
+
+
+def GetFakeDataset():
+    dataset = [
+        [1, 1, 'yes'],
+        [1, 1, 'yes'],
+        [1, 0, 'no'],
+        [0, 1, 'no'],
+        [0, 1, 'no'],
+    ]
+    labels = ['no surfacing', 'flippers']
+    # change to discrete values
+    return Dataset(dataset), labels
+
+
+class DicisionTree(object):
+    """ 决策树 """
+
+    def __init__(self, dataset, labels):
+        self.labels = labels
+        self.tree = self.BuildTree(Dataset(dataset), self.labels)
+
+    def SaveToFile(self, filename):
+        with open(filename, 'w') as outfile:
+            pickle.dump(self, outfile)
+
+    @staticmethod
+    def LoadFromFile(filename):
+        with open(filename, 'r') as infile:
+            tree = pickle.load(infile)
+        return tree
+
+    @staticmethod
+    def GetMajorityClass(classList):
+        classCount = defaultdict(int)
+        for vote in classList:
+            classCount[vote] += 1
+        sortedClassCount = sorted(
+            classCount.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )
+        return sortedClassCount[0][0]
+
+    def BuildTree(self, dataset, labels):
+        labels = labels[:]  # 复制防止破坏原来的 labels 列表
+
+        classList = dataset.classList
+        # 当子集中所有项都为同一 label , 直接返回
+        if classList.count(classList[0]) == len(classList): 
+            return classList[0]
+
+        # 当所有 feature 都用完, 返回出现次数最多的
+        _m, n = dataset.shape
+        if n == 1:
+            return self.GetMajorityClass(classList)
+
+        # 选择信息增益最大的进行划分
+        bestFeatureIndex = dataset.ChooseBestSplitFeature()
+        bestFeatureLabel = labels[bestFeatureIndex]
+        del(labels[bestFeatureIndex])
+        logging.info('Spliting by Feature {0}({1})'.format(
+            bestFeatureLabel,
+            bestFeatureIndex
+        ))
+
+        dicisionTree = {
+            bestFeatureLabel: {},
+        }
+
+        # 对特征下每个值进行递归划分
+        subLabels, subDatasets = dataset.split(bestFeatureIndex)
+        logging.info('labels:{0} for Feature {1}'.format(subLabels, bestFeatureLabel))
+        for subLabel, subDataset in zip(subLabels, subDatasets):
+            logging.info('Building subtree of value `{0}`'.format(subLabel))
+            dicisionTree[bestFeatureLabel][subLabel] = self.BuildTree(
+                subDataset,
+                labels
+            )
+            logging.info('Subtree `{0}` built'.format(subLabel))
+        return dicisionTree
+
+    def predict(self, inputVector):
+        return self.GetClassOfVector(self.tree, self.labels, inputVector)
+
+    def GetClassOfVector(self, dicisionTree, featureLabels, inputVector):
+        featureLabel = dicisionTree.keys()[0]
+        subDicisionTree = dicisionTree[featureLabel]
+        featureIndex = featureLabels.index(featureLabel)
+
+        downKey = inputVector[featureIndex]
+        downNode = subDicisionTree[downKey]
+
+        if isinstance(downNode, dict):
+            # 递归在子树中查找所属类别
+            classLabel = self.GetClassOfVector(
+                downNode, featureLabels,
+                inputVector
+            )
+        else:
+            classLabel = downNode
+        return classLabel
+
+
+def LoadLensesData(filename):
+    with open(filename) as infile:
+        lensesDataset = []
+        for line in infile:
+            trainVector = line.strip().split('\t')
+            lensesDataset.append(trainVector)
+        lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate', ]
+    lenseTree = DicisionTree(lensesDataset, lensesLabels)
+    return lenseTree
+
+if __name__ == '__main__':
+    tree = LoadLensesData('lenses.txt')
